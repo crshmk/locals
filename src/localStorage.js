f9bca551d4071fae28bb3390ba08f51f03aa4355
Chris Hammock <chris@crshmk.com>
2023-03-23 18:37:21 +0700

    add localStorage

---
+++ src/localStorage.js
@@ -0,0 +1,31 @@
+// assumes item to be set can be safely stringified 
+import { isNil } from 'ramda'
+
+/**
+  * @param String 
+  * @return any 
+*/
+const getItem = k => {
+  const item = localStorage.getItem(k)
+  return isNil(item) ? undefined : JSON.parse(item)
+}
+
+/**
+  * @param k String 
+  * @param v any 
+*/
+const setItem = (k, v) => {
+  v = JSON.stringify(v)
+  localStorage.setItem(k, v)
+}
+
+// @param k String 
+const unsetItem = k => {
+  localStorage.removeItem(k)
+}
+
+const localStorageUtil = {
+  getItem,
+  setItem,
+  unsetItem
+}
